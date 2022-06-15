2a75094d9b3e2107d6eafee1485d9387e02b07a4
Vasily Kulikov <azotbitzproduction@gmail.com>
2022-06-15 14:11:52 +0300

    HW3

--- azot-bitz-app/src/index.js
+++ azot-bitz-app/src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import SomeNestedChild from './App'
 import reportWebVitals from './reportWebVitals';
+import {ThemeProvider, createTheme} from '@material-ui/core';
+
+
+const theme = createTheme({
+    status: {
+        danger: '#e53e3e',
+    },
+    palette: {
+        type: 'light',
+        primary: {
+            main: '#3a7006',
+            dark: '#20340d',
+        },
+        neutral: {
+            main: '#64748B',
+            contrastText: '#282626',
+        },
+        background: {
+            default: '#ffffff',
+            paper: '#b5b5b5',
+        },
+    }});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
-    <App />
+    <ThemeProvider theme={theme}>
+        <SomeNestedChild />
+    </ThemeProvider>
 );
 
 reportWebVitals();
